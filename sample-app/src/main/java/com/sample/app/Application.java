e706989bb9016b93d397752d2ff9b0433bd5863a
facarvalho <facarvalho@paypal.com>
2017-07-26 16:07:28 -0500

    Configuring thread pool executor programatically

--- sample-app/src/main/java/com/sample/app/Application.java
+++ sample-app/src/main/java/com/sample/app/Application.java
@@ -1,8 +1,17 @@
 package com.sample.app;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.core.StandardThreadExecutor;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * SpringBoot entry point application
@@ -14,6 +23,34 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    private static final String TOMCAT_VI_THREAD_POOL_DOMAIN = "Tomcat";
+    private static final String TOMCAT_VI_THREAD_POOL_NAME = "AppThreadPool";
+    private static final String TOMCAT_VI_THREAD_POOL_NAME_PREFIX = "AppThreadPool-";
+
+    @Bean
+    public EmbeddedServletContainerCustomizer servletContainer() {
+        return (ConfigurableEmbeddedServletContainer container) -> {
+            ((TomcatEmbeddedServletContainerFactory) container).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+                @Override
+                public void customize(Connector connector) {
+                    StandardThreadExecutor executor = new StandardThreadExecutor();
+                    executor.setDomain(TOMCAT_VI_THREAD_POOL_DOMAIN);
+                    executor.setName(TOMCAT_VI_THREAD_POOL_NAME);
+                    executor.setNamePrefix(TOMCAT_VI_THREAD_POOL_NAME_PREFIX);
+                    executor.setMinSpareThreads(25);
+                    executor.setMaxThreads(40);
+                    Http11NioProtocol protocolHandler = (Http11NioProtocol) connector.getProtocolHandler();
+                    protocolHandler.setExecutor(executor);
+                    try {
+                        executor.start();
+                    } catch (LifecycleException e) {
+                        logger.error(e);
+                    }
+                }
+            });
+        };
     }
 
 }
